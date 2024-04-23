b69cbcb75ae1e779b1f8214dda98133cda16202e
Brittney Ferguson <bmferguson92@gmail.com>
2024-04-23 11:19:11 -0500

    mission to mars

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,28 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
+
+console.log(shuttleName);
+console.log(shuttleSpeedMph);
+console.log(distanceToMarsKm);
+console.log(distanceToMoonKm);
+console.log(milesPerKm);
+
+let milesToMars = distanceToMarsKm * milesPerKm;
+console.log(milesToMars);
+
+let hoursToMars = milesToMars / shuttleSpeedMph;
+console.log(hoursToMars);
+
+let daysToMars = hoursToMars / 24;
+console.log(daysToMars);
+
+console.log("Determination will take 332 days to reach Mars");
+
+
